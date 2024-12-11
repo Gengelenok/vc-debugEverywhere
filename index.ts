a56dc05ed343a56273da8f5c6f55be4098c1096a
Gengelenok <144037423+Gengelenok@users.noreply.github.com>
2024-12-11 07:18:55 +0100

    add index.ts

---
+++ index.ts
@@ -0,0 +1,110 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { getUserSettingLazy } from "@api/UserSettings"; // not del 50
+import { tryOrElse } from "@utils/misc"; // del isPluginDev save tryOrElse
+import { makeCodeblock } from "@utils/text"; // not del 103, 106
+import definePlugin from "@utils/types"; // not del
+
+import gitHash from "~git-hash"; // not del 67
+import plugins, { PluginMeta } from "~plugins"; // not del many
+
+import SettingsPlugin from "plugins/_core/settings.tsx"; // not del 68
+
+const ShowCurrentGame = getUserSettingLazy<boolean>("status", "showCurrentGame")!; // not del 79
+
+async function generateDebugInfoMessage() {
+    const { RELEASE_CHANNEL } = window.GLOBAL_ENV;
+
+    const client = (() => {
+        if (IS_DISCORD_DESKTOP) return `Discord Desktop v${DiscordNative.app.getVersion()}`;
+        if (IS_VESKTOP) return `Vesktop v${VesktopNative.app.getVersion()}`;
+        if ("legcord" in window) return `Legcord v${window.legcord.version}`;
+
+        // @ts-expect-error
+        const name = typeof unsafeWindow !== "undefined" ? "UserScript" : "Web";
+        return `${name} (${navigator.userAgent})`;
+    })();
+
+    const info = {
+        Vencord:
+            `v${VERSION} • [${gitHash}](<https://github.com/Vendicated/Vencord/commit/${gitHash}>)` +
+            `${SettingsPlugin.additionalInfo} - ${Intl.DateTimeFormat("en-GB", { dateStyle: "medium" }).format(BUILD_TIMESTAMP)}`,
+        Client: `${RELEASE_CHANNEL} ~ ${client}`,
+        Platform: window.navigator.platform
+    };
+
+    if (IS_DISCORD_DESKTOP) {
+        info["Last Crash Reason"] = (await tryOrElse(() => DiscordNative.processUtils.getLastCrash(), undefined))?.rendererCrashReason ?? "N/A";
+    }
+
+    const commonIssues = {
+        "NoRPC enabled": Vencord.Plugins.isPluginEnabled("NoRPC"),
+        "Activity Sharing disabled": tryOrElse(() => !ShowCurrentGame.getSetting(), false),
+        "Vencord DevBuild": !IS_STANDALONE,
+        "Has UserPlugins": Object.values(PluginMeta).some(m => m.userPlugin),
+        "More than two weeks out of date": BUILD_TIMESTAMP < Date.now() - 12096e5,
+    };
+
+    let content = `>>> ${Object.entries(info).map(([k, v]) => `**${k}**: ${v}`).join("\n")}`;
+    content += "\n" + Object.entries(commonIssues)
+        .filter(([, v]) => v).map(([k]) => `⚠️ ${k}`)
+        .join("\n");
+
+    return content.trim();
+}
+
+function generatePluginList() {
+    const isApiPlugin = (plugin: string) => plugin.endsWith("API") || plugins[plugin].required;
+
+    const enabledPlugins = Object.keys(plugins)
+        .filter(p => Vencord.Plugins.isPluginEnabled(p) && !isApiPlugin(p));
+
+    const enabledStockPlugins = enabledPlugins.filter(p => !PluginMeta[p].userPlugin);
+    const enabledUserPlugins = enabledPlugins.filter(p => PluginMeta[p].userPlugin);
+
+
+    let content = `**Enabled Plugins (${enabledStockPlugins.length}):**\n${makeCodeblock(enabledStockPlugins.join(", "))}`;
+
+    if (enabledUserPlugins.length) {
+        content += `**Enabled UserPlugins (${enabledUserPlugins.length}):**\n${makeCodeblock(enabledUserPlugins.join(", "))}`;
+    }
+
+    return content;
+}
+
+
+
+export default definePlugin({
+    name: "Debug Everywhere",
+    description: "Allows vencord-debug & vencord-plugins in all channels (but vc-debug & vc-plugins)",
+    authors: [{ name: "Mishonok", id: 860957238246899772n }],
+
+    commands: [
+        {
+            name: "vc-debug",
+            description: "Send Vencord debug info",
+            execute: async () => ({ content: await generateDebugInfoMessage() })
+        },
+        {
+            name: "vc-plugins",
+            description: "Send Vencord plugin list (by ",
+            execute: () => ({ content: generatePluginList() })
+        }
+    ],
+});
